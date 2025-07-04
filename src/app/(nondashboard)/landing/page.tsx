24fd719425ea90dab48d61d74f9e672270135f37
zahra <113920317+zesna79@users.noreply.github.com>
2025-07-04 20:45:25 +0330

    landing page - FeatureSection

--- src/app/(nondashboard)/landing/page.tsx
+++ src/app/(nondashboard)/landing/page.tsx
@@ -1,10 +1,12 @@
 import React from "react"
 import HeroSection from "./HeroSection"
+import FeaturesSection from "./FeaturesSection"
 
 const Landing = () => {
   return (
     <div>
       <HeroSection />
+      <FeaturesSection />
     </div>
   )
 }
